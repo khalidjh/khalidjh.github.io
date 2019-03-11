bc1b5a3329aad012adfb812a0207f85e3e2eb8ea
Khalid <khalidjh94@gmail.com>
2019-03-11 17:31:44 +0300

    connected firebase to app

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 $(document).ready(function(){
 
-    var prevArr = [];
+    // empty quiz object
+    var quiz;
+    var question_index = 0
     
-    //console.log(Object.keys(words2)[0]);
+    var questionColor = '';
+    var choicesColor = '';
+    var totalRounds = '';
+
+    initQuiz();
+
+    var prevArr = [];
     
     // ######## CONFIGURATIONS ########### //
     
     var currentRound = 0;
-    var totalRounds = 3;
     var score = 0;
     
-    var questionColor = "#429CC6";
     var correctColor = "#00cdbc";
     var wrongColor = "#cd0000";
     
-    var currentQuestion = 1
-    
     // ######## CONFIGURATIONS END ########### //
     
-    setQuestion();
-    
     $("#currentRound").text(currentRound);
-    $("#totalRounds").text(totalRounds);
     $("#scoreText").text(score);
         
-    $(".choice").click(function() {
+    $(document).on("click", ".choice", function() {
+        console.log("choice clicked");
         if($(this).attr("answer") == "correct" ) {
             score = score + 1;
             $("#scoreText").text(score);
             console.log("score "+score);
             $(this).animate({backgroundColor:correctColor}, 200);
             console.log("correct");
-            $("#c"+currentQuestion++).attr("src","img/1x/qg.png");
+            $("#c"+question_index).attr("src","img/1x/qg.png");
         } else {
             $(this).animate({backgroundColor:wrongColor}, 200);
             $("#choice1").animate({backgroundColor:correctColor}, 200);
             console.log("Wrong");
-            $("#c"+currentQuestion++).attr("src","img/1x/qr.png");
+            $("#c"+question_index).attr("src","img/1x/qr.png");
         }
         
         setTimeout(
@@ -55,6 +57,7 @@ $(document).ready(function(){
                 });
                 $(".choice").prop("disabled",true);
             } else {
+                question_index++;
                 setQuestion();
             }
         }, 1500);
@@ -70,93 +73,84 @@ $(document).ready(function(){
     );
     
     $("#restartBtn").click(function() {
-        currentQuestion = 1
-        $(".circles").empty()
+        $(".circles").empty();
         $(".choice").prop("disabled",false);
-        setQuestion();
         $(".scoreSection").fadeOut();
+
         $("#restartBtn").fadeOut("300", function () {
             $(".choices").fadeIn();
             $(".questionBody").fadeIn();
         });
+
         currentRound = 0;
         $("#currentRound").text(currentRound);
+
         score = 0;
         $("#score").text(score);
+
+        question_index = 0
+        setQuestion();
     });
+
+    function initQuiz() {
+        var database = firebase.firestore();
+        var quizId = "qc1fS6JmysGTfLTw7Ixd";
+
+        var docRef = database.collection("quizzes").doc(quizId);
+
+        docRef.get().then(function(doc) {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+                quiz = doc.data();
+
+                questionColor = quiz.question_color;
+                choicesColor = quiz.choices_color;
+                totalRounds = quiz.questions.length;
+
+                $(".upperSection").addClass(questionColor);
+
+                $("#totalRounds").text(totalRounds);
+                
+                console.log("quiz loaded");
+                setQuestion();
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+        });
+    }
     
     function setQuestion() {
+
+        // console.log(`quiz: ${JSON.stringify(quiz.questions[question_index])}`);
+        console.log("setting up Question " + question_index);
+
         // Add white question circle at top
-        $(".circles").append('<img id="c'+currentQuestion+'" src="img/1x/qw.png" />');
-        
-        // Get random question indexes
-        var numbers = getNumbers();
+        $(".circles").append('<img id="c'+question_index+'" src="img/1x/qw.png" />');
 
         // Set first index as the chosen question
-        var q = Object.keys(words2)[numbers[0]];
-        var a = Object.values(words2)[numbers[0]];
+        var q = quiz.questions[question_index].q;
+        var a = quiz.questions[question_index].a;
         console.log("q -> " + q);
         console.log("a -> " + a);
+
+        // Write the question
         $(".question").text(q);
-        
-        var arr = [0,1,2,3]
-        var firstTime = true
-        $(".choice").removeAttr("answer");
 
-        for(var i = arr.length-1;i>=0;i--) {
-          // Extract one number from 0 to 3
-          elem = arr.splice(Math.floor(Math.random()*arr.length), 1);
+        // Empty the choices
+        $('.choices').empty();
 
-          if(firstTime) {
-              // Set it as the question and answer
-              $("#choice"+elem[0]).text(a);
-              $("#choice"+elem[0]).attr("answer", "correct");
-              firstTime = false;
-          } else {
-              // Just set another choice for the other choices
-              c = Object.values(words2)[numbers[i+1]]
-              $("#choice"+elem[0]).text(c);
-          }
+        // Write the choices
+        for(var i = 0; i <= quiz.questions.length; i++) {
+            var choice = quiz.questions[question_index].choices[i];
+            $('.choices').append(`\
+                <div class="col-12">\
+                    <button answer="${i}" class="choice ${choicesColor}" id="choice${i}">${choice}</button>\
+                </div>\
+            `);
         }
     }
-   
-    function getNumbers() {
-        var arr = []
-        while(arr.length < 4){
-            var r = Math.floor(Math.random()*190) + 1;
-            if(arr.length == 0 && prevArr.indexOf(r) != -1){
-                continue;
-            }
-            if(arr.indexOf(r) === -1) {
-                arr.push(r);
-            }
-        }
-        return arr;
-    };
-    
-//    function nextQuestion() {
-//        numbers = getNumbers();
-//        q = Object.keys(words2)[numbers[0]];
-//        a = Object.values(words2)[numbers[0]];
-//        c1 = a;
-//        c2 = Object.values(words2)[numbers[1]];
-//        c3 = Object.values(words2)[numbers[2]];
-//        c4 = Object.values(words2)[numbers[3]];
-//
-////        console.log("q: "+q);
-////        console.log("a: "+a);
-////        console.log("c1: "+c1);
-////        console.log("c2: "+c2);
-////        console.log("c3: "+c3);
-////        console.log("c4: "+c4);
-//
-//        $(".question").text(q);
-//        $("#choice1").text(c1);
-//        $("#choice2").text(c2);
-//        $("#choice3").text(c3);
-//        $("#choice4").text(c4);
-//        
-//        $(".circles").append('<img id="c'+currentQuestion+'" src="img/1x/qw.png" />')
-//    }
     
 });
